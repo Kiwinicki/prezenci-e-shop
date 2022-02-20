755f098abd3d6f58f22b5ec12beed00d703703c0
DESKTOP-2P9DAPD\Windows 10 <j.kiwinicki@wp.pl>
2022-02-20 14:37:53 +0100

    changes for correct routes in react-router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import store from './store';
-import { Provider as StoreProvider } from 'react-redux';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./App";
+import store from "./store";
+import { Provider as StoreProvider } from "react-redux";
+import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<StoreProvider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</StoreProvider>
 	</React.StrictMode>,
-	document.getElementById('root')
+	document.getElementById("root")
 );
